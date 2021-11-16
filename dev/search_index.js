84282fb3984c8138fa0532f32300d3c55ee61cea
Documenter.jl <documenter@juliadocs.github.io>
2021-11-16 20:29:27 +0000

    build based on a83a505

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MathepiaInference","category":"page"},{"location":"#MathepiaInference","page":"Home","title":"MathepiaInference","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MathepiaInference.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MathepiaInference]","category":"page"}]
+}
